18d19bc989668f3f1b319f48d94f844afba6e4bd
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-08-25 20:44:05 -0300

    F4.2 class 22 complete

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -27,32 +27,10 @@ module.exports = {
             fs.unlinkSync(file.path)
 
             return db.query(`
-            DELETE FROM files WHERE id = $1
-            `, [id])
+                DELETE FROM files WHERE id = $1
+                `, [id])
         } catch(err) {
             console.log(err)
         }
     }
 }
-
-/*VERSAO ERRADA MOSTRDA EM AULAS
-
-module.exports = {
-    create(filename, path, product_id) {
-        const query = `
-            INSERT INTO files (
-                name,
-                path,
-                product_id
-            ) VALUES ($1, $2, $3)
-            RETURNING id
-        `
-        const values = [
-            filename,
-            path,
-            product_id
-        ]
-
-        return db.query(query, values)
-    }
-}*/
