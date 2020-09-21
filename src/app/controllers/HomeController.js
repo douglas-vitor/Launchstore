fcc87d4732993c5759b3b5ca4124e7d02af55e26
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-09-21 19:17:41 -0300

    Update homeController product.image replace

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -13,19 +13,20 @@ module.exports = {
 
         async function getImage(productId) {
         let results = await Product.files(productId)
-        const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+        const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "").replace("\\", "\/").replace("\\", "\/")}`)
 
         return files[0]
         }
 
         const productsPromise = Products.map(async product => {
-            product.img = await getImage(product.id)
-            product.oldprice = formatPrice(product.old_price)
+            product.image = await getImage(product.id)
+            product.oldPrice = formatPrice(product.old_price)
             product.price = formatPrice(product.price)
             return product
         }).filter((product, index) => index > 2 ? false : true)
 
         const lastAdded = await Promise.all(productsPromise)
+
         return res.render("home/index", { products: lastAdded })
     }   
 }
