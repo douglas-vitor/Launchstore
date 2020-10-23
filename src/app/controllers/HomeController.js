cf6d11b6684160f62763afbb57047e2c02216aa2
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-10-23 19:50:43 -0300

    F4 - listando os produtos complete

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -4,29 +4,33 @@ const Product = require("../models/Product")
 
 module.exports = {
     async index(req, res) {
-        let results = await Product.all()
-        const Products = results.rows
+        try {
+            let results = await Product.all()
+            const Products = results.rows
 
-        if(!Products) {
-            return res.send("Products not found!")
+            if (!Products) {
+                return res.send("Products not found!")
+            }
+
+            async function getImage(productId) {
+                let results = await Product.files(productId)
+                const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "").replace("\\", "\/").replace("\\", "\/")}`)
+
+                return files[0]
+            }
+
+            const productsPromise = Products.map(async product => {
+                product.image = await getImage(product.id)
+                product.oldPrice = formatPrice(product.old_price)
+                product.price = formatPrice(product.price)
+                return product
+            }).filter((product, index) => index > 2 ? false : true)
+
+            const lastAdded = await Promise.all(productsPromise)
+
+            return res.render("home/index", { products: lastAdded })
+        } catch (err) {
+            console.log(err)
         }
-
-        async function getImage(productId) {
-        let results = await Product.files(productId)
-        const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "").replace("\\", "\/").replace("\\", "\/")}`)
-
-        return files[0]
-        }
-
-        const productsPromise = Products.map(async product => {
-            product.image = await getImage(product.id)
-            product.oldPrice = formatPrice(product.old_price)
-            product.price = formatPrice(product.price)
-            return product
-        }).filter((product, index) => index > 2 ? false : true)
-
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("home/index", { products: lastAdded })
-    }   
+    }
 }
