d098c52b6c51ceab8f7f60e0b15c93878b106b22
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-07 11:56:39 -0300

    Update seed

--- src/config/database.sql
+++ src/config/database.sql
@@ -105,3 +105,12 @@ ADD CONSTRAINT files_product_id_fkey
 FOREIGN KEY ("product_id") 
 REFERENCES "products" ("id") 
 ON DELETE CASCADE;
+
+--restart sequence auto_increment from tables ids
+DELETE FROM products;
+DELETE FROM users;
+DELETE FROM files;
+
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
+ALTER SEQUENCE files_id_seq RESTART WITH 1;
