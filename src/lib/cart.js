c4a74cc4cc8ace8cf9886c694a2bc61ceb981467
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2020-11-22 10:18:37 -0300

    Update cart

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -16,7 +16,7 @@ const Cart = {
         return this
     },
     addOne(product) {
-        let inCart = this.items.find(item => item.product.id == product.id)
+        let inCart = this.getCartItem(product.id)
         if(!inCart) {
             inCart = {
                 product: {
@@ -45,7 +45,7 @@ const Cart = {
     },
     removeOne(productId) {
         //pegar item do carrinho
-        const inCart = this.items.find(item => item.product.id == productId)
+        const inCart = this.getCartItem(productId)
 
         if(!inCart) return this
 
@@ -68,36 +68,22 @@ const Cart = {
 
         return this
     },
-    delete(productId) {}
+    delete(productId) {
+        const inCart = this.getCartItem(productId)
+        if(!inCart) return this
+
+        if(this.items.length > 0) {
+            this.total.quantity -= inCart.quantity
+            this.total.price -= (inCart.product.price * inCart.quantity)
+            this.total.formatedPrice = formatPrice(this.total.price)
+        }
+
+        this.items = this.items.filter(item => inCart.product.id != item.product.id)
+        return this
+    },
+    getCartItem(productId) {
+        return this.items.find(item => item.product.id == productId)
+    }
 }
 
-const product = {
-    id: 1,
-    price: 199,
-    quantity: 2
-}
-const product2 = {
-    id: 2,
-    price: 229,
-    quantity: 1
-}
-console.log('add first product')
-let oldCart = Cart.init().addOne(product)
-console.log(oldCart)
-
-console.log('add second product')
-oldCart = Cart.init(oldCart).addOne(product)
-console.log(oldCart)
-
-console.log('add thirt product')
-oldCart = Cart.init(oldCart).addOne(product2)
-console.log(oldCart)
-
-
-console.log('remove onde item')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
-console.log('remove onde item')
-oldCart = Cart.init(oldCart).removeOne(product.id)
-console.log(oldCart)
 module.exports = Cart
