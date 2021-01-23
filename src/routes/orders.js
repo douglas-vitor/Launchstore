e407a6c578a349915c0c1101d2d830a3d9a9d2a0
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2021-01-23 17:53:01 -0300

    Service de pedidos

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -8,6 +8,7 @@ const { onlyUsers } = require("../app/middlewares/session")
 
 routes.post('/', onlyUsers, OrderController.post)
     .get('/', onlyUsers, OrderController.index)
+    .get('/sales', onlyUsers, OrderController.sales)
 
 
 module.exports = routes
